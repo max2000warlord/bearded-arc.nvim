fd1951dd30a4640664fcc7ba1f6fa5e71c6fae15
Matthew Anderson <mpa.business@pm.me>
2025-11-11 17:52:05 +1100

    update

--- lua/bearded-arc/config.lua
+++ lua/bearded-arc/config.lua
@@ -2,13 +2,20 @@ local M = {}
 
 M.options = {
 	transparent = false,
+	transparent_sidebar = false, -- Only make sidebars transparent
 	terminal_colors = true,
+	dim_inactive = false, -- Dim inactive windows
 	styles = {
 		comments = { italic = true },
-		keywords = {},
+		keywords = { italic = false },
 		functions = {},
 		variables = {},
+		sidebars = "dark", -- "dark", "transparent", "normal"
+		floats = "dark", -- "dark", "transparent", "normal"
 	},
+	sidebars = { "qf", "help", "vista_kind", "terminal", "packer" },
+	hide_inactive_statusline = false,
+	lualine_bold = false,
 }
 
 function M.setup(opts)
