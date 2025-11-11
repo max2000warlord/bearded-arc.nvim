1aa7e97eac753541c0a81a58340891e6efbe8a92
Matthew Anderson <mpa.business@pm.me>
2025-11-11 18:30:52 +1100

    update

--- lua/bearded-arc/config.lua
+++ lua/bearded-arc/config.lua
@@ -17,3 +17,9 @@ M.options = {
 	hide_inactive_statusline = false,
 	lualine_bold = false,
 }
+
+function M.setup(opts)
+	M.options = vim.tbl_deep_extend("force", M.options, opts or {})
+end
+
+return M
